0a28d1209709172622a71da3c92a63742c446bb5
Lukas Walter <lukas.h.walter@gmail.com>
2026-05-10 19:14:41 +0200

    Adds generate_legal_moves_pawn,check_if_square_is_in_bounds, check_if_square_is_occupied functions, adds new Move Struct

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,26 @@ fn main(){
         ]
     ];
 
-    generate_legal_moves_pawn(&board, 4,7);
+    let mut all_legal_moves : Vec<Move> = Vec::new();
+
+
+    for row in 0..8{
+        for col in 0..8{
+            if let Some(piece) = board[row][col]{
+
+                let moves = match piece.kind{
+                    PieceType::Pawn => generate_legal_moves_pawn(&board, row, col),
+                    _ => Vec::new(),
+                };
+
+                all_legal_moves.extend(moves);
+
+            }
+        }
+    }
+
+    println!("{:#?}", all_legal_moves);
+
 }
 
 
@@ -70,65 +89,89 @@ fn p (kind : PieceType, is_white : bool) -> Option<Piece> {
     Some(Piece{kind, is_white})
 }
 
-fn generate_legal_moves_pawn(board : &[[Option<Piece>; 8]; 8] , col : usize, row : usize) ->Vec<(usize, usize)>{
-    let piece = board[row][col];
-    let mut legal_moves: Vec<(usize, usize)> = Vec::new();
+fn generate_legal_moves_pawn(board : &[[Option<Piece>; 8]; 8] , row : usize, col : usize) -> Vec<Move>{
+    let mut legal_moves: Vec<Move> = Vec::new();
     
     
     if let Some(piece) = board[row][col] {
        
-        let direction = if piece.is_white {-1} else {1}; 
-
-        if piece.is_white{
-            let capture_left = (row + 1, col-1);
-            let capture_right = (row + 1, col+1);
-            let move_1sqr_forward = (row+1, col);
-            let move_2sqrs_forward = (row+2, col);
-
-            if !check_if_square_is_occupied(board, move_1sqr_forward){
-                legal_moves.push(move_1sqr_forward);
-            }
-
-            if !check_if_square_is_occupied(board, move_1sqr_forward) &&
-               !check_if_square_is_occupied(board, move_2sqrs_forward){
-                legal_moves.push(move_2sqrs_forward);
-            }
-
-
-        } else{
-            let capture_left = (row-1, col-1);
-            let capture_right = (row-1, col+1);
-            let move_1sqr_forward = (row-1, col);
-            let move_2sqrs_forward = (row-2, col);
-
-            if !check_if_square_is_occupied(board, move_1sqr_forward){
-                legal_moves.push(move_1sqr_forward);
-            }
-
-            if !check_if_square_is_occupied(board, move_1sqr_forward){
-                legal_moves.push(move_2sqrs_forward);
-            }
-        }
-    
+        let direction : isize = if piece.is_white {-1} else {1}; 
+        let start_row = if piece.is_white{6} else{1};
 
       
-    } else {
-    
-        
-        
-    
-    }
+            //let capture_left = (row + 1, col-1);
+            //let capture_right = (row + 1, col+1);
+
+            
+        let move_1sqr_forward = (row as isize + direction, col as isize);
+
+        if check_if_square_is_in_bounds(move_1sqr_forward) {
+            let move_1sqr_forward = (move_1sqr_forward.0 as usize, move_1sqr_forward.1 as usize);
+
+            if !check_if_square_is_occupied(board, move_1sqr_forward){
+                let current_move : Move = Move { from_row: row, from_col: col, to_row: move_1sqr_forward.0, to_col: move_1sqr_forward.1 };
+                legal_moves.push(current_move);
+            }    
+
+            let move_2sqrs_forward = (row as isize + 2 * direction, col as isize);
+
+            if check_if_square_is_in_bounds(move_2sqrs_forward) && row == start_row{
+                
+                let move_2sqrs_forward = (move_2sqrs_forward.0 as usize, move_2sqrs_forward.1 as usize);
+                
+                if !check_if_square_is_occupied(board, move_2sqrs_forward){
+
+                    let current_move : Move = Move { from_row: row, from_col: col, to_row: move_2sqrs_forward.0, to_col: move_2sqrs_forward.1 };
+                    legal_moves.push(current_move);
+                } 
+            }
+        }
+         
+
+      
+    } 
 
     legal_moves
    
 }
 
+
+fn check_if_square_is_in_bounds (square : (isize, isize)) -> bool{
+    square.0 >= 0 &&
+    square.0 < 8 &&
+    square.1 >= 0 &&
+    square.1 < 8
+}
+  
 fn check_if_square_is_occupied(board : &[[Option<Piece>; 8]; 8] ,square :  (usize, usize)) -> bool{
     let (row, col) = square;
 
-    if let Some(piece) = board[row][col]{
+    if let Some(_) = board[row][col]{
         true
     }else{
         false
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+
+struct Move{
+    from_row : usize,
+    from_col : usize,
+    to_row : usize,
+    to_col : usize
+}
+
+struct Move_In_Chess_Notation{
+    from_row : usize,
+    from_col : usize,
+    to_row : usize,
+    to_col : usize
+}
+
+
+impl Move{
+    fn translate_move_to_chess_notation (self) {
+
     }
 }
